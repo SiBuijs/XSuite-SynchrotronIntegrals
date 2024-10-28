873745b1103919278c6740810465caef6ea9cff6
SiBuijs <simon.fanica.buijsman@cern.ch>
2024-10-28 11:32:52 +0100

    Just a test.

--- synchrotron_integrals.py
+++ synchrotron_integrals.py
@@ -29,6 +29,7 @@
 #### ---------------------------------------------------------------------------------------------------------------------------------
 import numpy as np
 
+# NOTE: This is a test to test the if the commit goes through as expected.
 
 # Physical constants required to calculate the physical quantities associated with the radiation integrals.
 from scipy.constants import c as clight, hbar, electron_volt
