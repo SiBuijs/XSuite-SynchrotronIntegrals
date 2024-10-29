d6fad6c86e97cf4b0f0ad78ea397c9e9787faf48
Simon Buijsman <simon.fanica.buijsman@cern.ch>
2024-10-29 10:08:44 +0100

    Changed the gitignore a bit.

--- test_SPS_Electrons_Vertical_Bend.py
+++ test_SPS_Electrons_Vertical_Bend.py
@@ -1,104 +1,115 @@
+import numpy as np
+from scipy.constants import c as clight
+from scipy.constants import hbar
+from scipy.constants import epsilon_0
+
+from cpymad.madx import Madx
 import xtrack as xt
+import xobjects as xo
 
-from ..synchrotron_integrals import SynchrotronIntegral as synint
+mad = Madx()
+mad.call('sps.seq')
 
-line = xt.Line.from_json('line.json')
-line.particle_ref = xt.Particles(energy0=20e9, mass0=xt.ELECTRON_MASS_EV)
+# mad.input('beam, particle=proton, pc=26;')
+# mad.input('beam, particle=electron, pc=20;')
+
+# # realistic
+# mad.input('beam, particle=electron, pc=20;')
+# v_mv = 25
+# num_turns = 8000
+
+# higher energy
+mad.input('beam, particle=electron, pc=50;')
+v_mv = 250
+num_turns = 600
+
+mad.call('../../test_data/sps_thick/lhc_q20.str')
+
+mad.use(sequence='sps')
+
+mad.input('twiss, table=tw4d;')
+twm4d = mad.table.tw4d
+
+n_cav = 6
+
+mad.sequence.sps.elements['actcse.31632'].volt = v_mv * 10 / n_cav   # To stay in the linear region
+mad.sequence.sps.elements['actcse.31632'].freq = 3
+mad.sequence.sps.elements['actcse.31632'].lag = 0.5
+
+
+mad.input('twiss, table=tw6d;')
+twm6d = mad.table.tw6d
+
+mad.sequence.sps.beam.radiate = True
+mad.emit()
+
+line = xt.Line.from_madx_sequence(mad.sequence.sps, allow_thick=True,
+                                  deferred_expressions=True)
+line.particle_ref = xt.Particles(mass0=xt.ELECTRON_MASS_EV,
+                                    q0=-1, gamma0=mad.sequence.sps.beam.gamma)
+line.cycle('bpv.11706', inplace=True)
+
+line.insert_element(element=line['actcse.31632'].copy(), index='bpv.11706',
+                    name='cav1')
+line.insert_element(element=line['actcse.31632'].copy(), index='bpv.21508',
+                    name='cav2')
+line.insert_element(element=line['actcse.31632'].copy(), index='bpv.41508',
+                    name='cav4')
+line.insert_element(element=line['actcse.31632'].copy(), index='bpv.51508',
+                    name='cav5')
+line.insert_element(element=line['actcse.31632'].copy(), index='bpv.61508',
+                    name='cav6')
+
+tt = line.get_table()
+
+s_start_wig = tt['s', 'actcsg.31780']
+
+line.discard_tracker()
+line.insert_element(name='wig1', element=xt.Bend(length=0.5, rot_s_rad=np.pi/2), at_s=s_start_wig+0.1+0*0.5)
+line.insert_element(name='wig2', element=xt.Bend(length=0.5, rot_s_rad=np.pi/2), at_s=s_start_wig+0.1+1*0.5)
+line.insert_element(name='wig3', element=xt.Bend(length=0.5, rot_s_rad=np.pi/2), at_s=s_start_wig+0.1+2*0.5)
+line.insert_element(name='wig4', element=xt.Bend(length=0.5, rot_s_rad=np.pi/2), at_s=s_start_wig+0.1+3*0.5)
+line.insert_element(name='wig5', element=xt.Bend(length=0.5, rot_s_rad=np.pi/2), at_s=s_start_wig+0.1+4*0.5)
+line.insert_element(name='wig6', element=xt.Bend(length=0.5, rot_s_rad=np.pi/2), at_s=s_start_wig+0.1+5*0.5)
+line.insert_element(name='wig7', element=xt.Bend(length=0.5, rot_s_rad=np.pi/2), at_s=s_start_wig+0.1+6*0.5)
+line.insert_element(name='wig8', element=xt.Bend(length=0.5, rot_s_rad=np.pi/2), at_s=s_start_wig+0.1+7*0.5)
+
+line['k_wig'] = 0
+line['wig1'].k0 = 'k_wig'
+line['wig2'].k0 = '-k_wig'
+line['wig3'].k0 = '-k_wig'
+line['wig4'].k0 = 'k_wig'
+line['wig5'].k0 = '-k_wig'
+line['wig6'].k0 = 'k_wig'
+line['wig7'].k0 = 'k_wig'
+line['wig8'].k0 = '-k_wig'
+
+line['k_wig'] = 1e-3
+tw = line.twiss4d()
+
+# Remove edge effects
+# for nn in tt.rows[tt.element_type=='DipoleEdge'].name:
+#     line[nn].k = 0
+
+tw_thick = line.twiss()
+
+Strategy = xt.slicing.Strategy
+Teapot = xt.slicing.Teapot
 
 line.discard_tracker()
 slicing_strategies = [
-    xt.Strategy(slicing=xt.Teapot(1)),  # Default
-    xt.Strategy(slicing=xt.Teapot(2), element_type=xt.Bend),
-    xt.Strategy(slicing=xt.Teapot(8), element_type=xt.Quadrupole),
+    Strategy(slicing=Teapot(1)),  # Default
+    Strategy(slicing=Teapot(2), element_type=xt.Bend),
+    Strategy(slicing=Teapot(8), element_type=xt.Quadrupole),
+    Strategy(slicing=Teapot(20), name='wig.*'),
 ]
-line.slice_thick_elements(slicing_strategies)
 
-tw_no_rad = line.twiss(strengths=True)
+line.slice_thick_elements(slicing_strategies)
+line.build_tracker()
+
+line.discard_tracker()
+line.build_tracker(_context=xo.ContextCpu())
+line.configure_radiation(model=None)
 
 line.configure_radiation(model='mean')
-
-tw_rad = line.twiss(strengths=True, eneloss_and_damping=True)
-
-print(tw_rad.keys())
-
-# Compute radiation integrals
-Integrals = synint(line)
-
-# Momentum Compaction Factor
-# NOTE: CORRECT
-alpha_cI = Integrals.momentum_compaction()
-alpha_cX = tw_rad.momentum_compaction_factor
-print("\n-----------------------------------------------------------------------------------")
-print(f"Momentum Compaction Factors:\nalpha_cIx = {alpha_cI[0]} [-]")
-print(f"alpha_cIy = {alpha_cI[1]} [-]")
-print(f"alpha_xs = {alpha_cX} [-]\n")
-print(f"alpha_xs/alpha_cIx - 1 = {alpha_cI[0]/alpha_cX - 1:2e}")
-
-# Energy loss
-# NOTE: CORRECT
-U_0I = Integrals.energy_loss()
-U_0X = tw_rad.eneloss_turn
-
-print("\n-----------------------------------------------------------------------------------")
-print(f"Energy Loss:\nU_0I = {U_0I} [eV]")
-print(f"U_0X = {U_0X} [eV]\n")
-print(f"U_0I/U_0X - 1 = {U_0I/U_0X - 1:2e}")
-
-# Radiation damping [s⁻¹]
-# NOTE: CORRECT
-alpha_xIs = Integrals.radiation_damping_s()[0]
-alpha_yIs = Integrals.radiation_damping_s()[1]
-alpha_sIs = Integrals.radiation_damping_s()[2]
-
-alpha_xXs = tw_rad.damping_constants_s[0]
-alpha_yXs = tw_rad.damping_constants_s[1]
-alpha_sXs = tw_rad.damping_constants_s[2]
-
-RelErr_xs = alpha_xIs/alpha_xXs - 1
-RelErr_ys = alpha_yIs/alpha_yXs - 1
-RelErr_ss = alpha_sIs/alpha_sXs - 1
-
-print("\n-----------------------------------------------------------------------------------")
-print(f"Radiation Damping (Integral Method):\nalpha_x = {alpha_xIs} [s⁻¹]\nalpha_y = {alpha_yIs} [s⁻¹]\nalpha_s = {alpha_sIs} [s⁻¹]\n")
-print(f"Radiation Damping (XSuite Method):\nalpha_x = {alpha_xXs} [s⁻¹]\nalpha_y = {alpha_yXs} [s⁻¹]\nalpha_s = {alpha_sXs} [s⁻¹]\n")
-print(f"Relative Errors:\nalpha_xI/alpha_xX - 1 = {RelErr_xs:2e}\nalpha_xI/alpha_xX - 1 = {RelErr_ys:2e}\nalpha_sI/alpha_sX - 1 = {RelErr_ss:2e}\n")
-
-# Radiation damping [turns⁻¹]
-# NOTE: CORRECT
-alpha_xIt = Integrals.radiation_damping_turns()[0]
-alpha_yIt = Integrals.radiation_damping_turns()[1]
-alpha_sIt = Integrals.radiation_damping_turns()[2]
-
-alpha_xXt = tw_rad.damping_constants_turns[0]
-alpha_yXt = tw_rad.damping_constants_turns[1]
-alpha_sXt = tw_rad.damping_constants_turns[2]
-
-RelErr_xt = alpha_xIt/alpha_xXt - 1
-RelErr_yt = alpha_yIt/alpha_yXt - 1
-RelErr_st = alpha_sIt/alpha_sXt - 1
-
-print("\n-----------------------------------------------------------------------------------")
-print(f"Radiation Damping (Integral Method):\nalpha_x = {alpha_xIt} [s⁻¹]\nalpha_y = {alpha_yIt} [s⁻¹]\nalpha_s = {alpha_sIt} [turns⁻¹]\n")
-print(f"Radiation Damping (XSuite Method):\nalpha_x = {alpha_xXt} [s⁻¹]\nalpha_y = {alpha_yXt} [s⁻¹]\nalpha_s = {alpha_sXt} [turns⁻¹]\n")
-print(f"Relative Errors:\nalpha_xI/alpha_xX - 1 = {RelErr_xt:2e}\nalpha_xI/alpha_xX - 1 = {RelErr_yt:2e}\nalpha_sI/alpha_sX - 1 = {RelErr_st:2e}\n")
-
-# Quantum excitation in the longitudinal direction
-eq_emittanceI = Integrals.equilibrium_emittance()
-eq_emittanceX = tw_rad.eq_gemitt_zeta
-print("\n-----------------------------------------------------------------------------------")
-print(f"eq_emittanceI = {eq_emittanceI} [m]")
-print(f"eq_emittanceX = {eq_emittanceX} [m]\n")
-print(f"eq_emittanceI/eq_emittanceX - 1 = {eq_emittanceI/eq_emittanceX - 1:2e}")
-
-# Quantum excitation in the radial direction
-# NOTE: CORRECT
-rms_betatronIx = Integrals.rms_betatron()[0]
-rms_betatronIy = Integrals.rms_betatron()[1]
-rms_betatronX = tw_rad.eq_gemitt_x
-print("\n-----------------------------------------------------------------------------------")
-print(f"rms_betatronIx = {rms_betatronIx} [-]")
-print(f"rms_betatronIy = {rms_betatronIy} [-]")
-print(f"rms_betatronX = {rms_betatronX} [-]\n")
-print(f"rms_betatronIx/rms_betatronX - 1 = {rms_betatronIx/rms_betatronX - 1:2e}")
-
-print()
+twr = line.twiss(eneloss_and_damping=True)
